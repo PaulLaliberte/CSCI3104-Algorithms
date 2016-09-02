b077a786696ad7d349b204422881308a55a9d8e8
Paul Laliberte <Paul@reshut1-33-201-dhcp.int.colorado.edu>
2016-09-02 02:02:48 -0600

    cleaned up code

--- written_assign1/p1.py
+++ written_assign1/p1.py
@@ -1,32 +1,21 @@
 def findCommonElement(array1, array2):
     i=0
     j=0
-    count = 0
-    
+
     while i < len(array1) and j < len(array2):
-        if array1[i] < array2[j]:
+        if array1[i] is array2[j]:
+            return True
+        elif array1[i] < array2[j]:
             i = i + 1
-        elif array1[i] is array2[j]:
-            print("The number %i is common." % array1[i])
-            i = i + 1
-            j = j + 1
-            count = count + 1
         else:
-            mergedArray.append(array2[j])
             j = j + 1
-    print("Number of common elements is: %i" % count)
-
-"""
-    while i < len(array1):
-        mergedArray.append(array1[i])
-        i = i + 1
-
-    while j < len(array2):
-        mergedArray.append(array2[j])
-        j = j + 1
+    return False
 
 
-    return mergedArray
-"""
-
-
+#Tests
+a = [3,5,456,23324]
+b = [1,2,3,4]
+c = [1,2,3,4,5]
+d = [6,7,8,9,10]
+print(findCommonElement(a,b))
+print(findCommonElement(c,d))
