f08e0305f02074ba8859a1b9b720be7551e4bd33
Paul Laliberte <pala5552@colorado.edu>
2016-10-18 04:22:52 -0600

    updated 1st func, passes all newtests

--- program_assign2/programming_assignment_2_kit/pa2solution.py
+++ program_assign2/programming_assignment_2_kit/pa2solution.py
@@ -14,12 +14,80 @@ import random
 
 
 def free_time_intervals(interval_lst, T):
-    # First design the algorithm on pen/paper before you attempt this
-    return None
+    missing = []
+    interval_lst.sort()                                     #O(nlogn)
+    x = 0
+    if interval_lst[0][0] > x:
+        missing.append((0, interval_lst[0][0]))             #1. check if start time missing
+                                                            #O(1)
+    y = interval_lst[0][1]
+    i = 1
 
-def max_logged_in(interval_lst,T):
-    # First design the algorithm on pen/paper and solve a few examples.
-    return None
+    for i in range(1, len(interval_lst)):                 #2. check internal times 
+        if interval_lst[i][0] not in range(x,y+1):                       #O(n)
+            if interval_lst[i][0] > T:
+                missing.append((y, T))
+            else:
+                missing.append((y, interval_lst[i][0]))
+                y = interval_lst[i][1]
+        else:
+            if interval_lst[i][1] > y:
+                y = interval_lst[i][1]
+
+
+    max_second_ele = 0
+    for i in interval_lst:
+        max_tuple = max(interval_lst, key=lambda x:x[1])
+        max_second_ele = max_tuple[1]
+
+    if max_second_ele < T:
+        missing.append((y,T))
+
+    missing.sort()                                          #O(nlogn)
+
+
+
+    #Total Time: T(n) = 2O(nlogn) + O(n) + 2O(1)
+    #                 = O(nlogn)
+
+
+
+    return missing
+
+
+
+def max_logged_in(interval_lst, T):
+    max_time = []
+    interval_lst.sort()
+    count = 1
+    x = interval_lst[0][0]
+    y = interval_lst[0][1]
+    time_int = x
+    i = 1
+
+    if len(interval_lst) > 1:
+        for i in range(1, len(interval_lst)):
+            if interval_lst[i][0] <= y and interval_lst[i][1] in range(x,y+1):
+                count += 1
+                if interval_lst[i][1] > x:
+                    time_int = interval_lst[i][0]
+            elif interval_lst[i][0] <= y and interval_lst[i][1] not in range(x, y+1) and interval_lst[i][0] == x:
+                count += 1
+                y = interval_lst[i][1]
+            else:
+                max_time.append((count, time_int))
+                x = interval_lst[i][0]
+                y = interval_lst[i][1]
+                count = 1 
+                time_int = x
+    else:
+        max_time.append((count, time_int))
+
+
+    tup = max(max_time)
+
+    return tup
+
 
 
 
