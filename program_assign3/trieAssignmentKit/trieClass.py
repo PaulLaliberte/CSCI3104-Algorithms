e207d6315d615bf5383918b80932fabfdce870e7
Paul Laliberte <pala5552@colorado.edu>
2016-11-03 03:11:23 -0600

    DFS for autocomplete

--- program_assign3/trieAssignmentKit/trieClass.py
+++ program_assign3/trieAssignmentKit/trieClass.py
@@ -1,24 +1,18 @@
-#LastName:
-#FirstName:
-#Email:
-#Comments:
+#LastName: Laliberte'
+#FirstName: Paul
+#Email: pala5552@colorado.edu
+#Comments: DFS implemented
 
 from __future__ import print_function
 import sys
 import copy
-import collections
 
-# We will use a class called my trie node
 class MyTrieNode:
-    # Initialize some fields 
   
     def __init__(self, isRootNode):
-        #The initialization below is just a suggestion.
-        #Change it as you will.
-        # But do not change the signature of the constructor.
         self.isRoot = isRootNode
-        self.count = 0 # frequency count
-        self.next = {} # Dictionary mappng each character from a-z to the child node
+        self.count = 0 
+        self.next = {} 
 
 
     def addWord(self,w):
@@ -34,8 +28,6 @@ class MyTrieNode:
             self.count += 1
 
 
-        
-
     def lookupWord(self, w):
         if len(w) != 0:
             if w[0] not in self.next:
@@ -47,20 +39,41 @@ class MyTrieNode:
             return self.count
 
 
+    def depthFirstSearch(self, char, array):
+        if self.count > 0:
+            array.append((char, self.count))
+
+        if len(self.next.keys()) == 0:
+            return
+
+        for k in self.next.keys():
+            self.next[k].depthFirstSearch(char+k, array)
 
 
-    def autoComplete(self,w):
-        #Returns possible list of autocompletions of the word w
-        #Returns a list of pairs (s,j) denoting that
-        #         word s occurs with frequency j
+    def autoComplete(self, w, array=[], char=''):
+        if len(array) > 0:
+            array = []
 
-        #YOUR CODE HERE
+        if len(w) != 0:
+            if w[0] not in self.next:
+                array = []
+                return array
+            else:
+                self = self.next[w[0]]
+                char += w[0]
+                return self.autoComplete(w[1:], array, char)
         
-        return [('Walter',1),('Mitty',2),('Went',3),('To',4),('Greenland',2)] #TODO: change this line, please
-    
-    
-            
-"""
+        else:
+            if self.count > 0:
+                array.append((char, self.count))
+
+            for k in self.next.keys():
+                self.next[k].depthFirstSearch(char+k, array)
+
+            return array
+
+      
+
 if (__name__ == '__main__'):
     t= MyTrieNode(True)
     lst1=['test','testament','testing','ping','pin','pink','pine','pint','testing','pinetree']
@@ -72,14 +85,15 @@ if (__name__ == '__main__'):
     j2 = t.lookupWord('telltale') # should return 0
     j3 = t.lookupWord ('testing') # should return 2
     print(j, j2, j3)
-    #lst3 = t.autoComplete('pi')
-    #print('Completions for \"pi\" are : ')
-    #print(lst3)
+    lst3 = t.autoComplete('pi')
+    print('Completions for \"pi\" are : ')
+    print(lst3)
     
-    #lst4 = t.autoComplete('tes')
-    #print('Completions for \"tes\" are : ')
-    #print(lst4)
-"""
+    lst4 = t.autoComplete('tes')
+    print('Completions for \"tes\" are : ')
+    print(lst4)
+
+
 
 
  
